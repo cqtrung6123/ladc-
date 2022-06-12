c0bfc4e1a142ed17e6058646ca8debfb612f5ec1
cqtrung6123 <93904342+cqtrung6123@users.noreply.github.com>
2022-06-12 21:39:55 +0700

    bt12/6

---
+++ lad5.1/ConnectionDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lad5._1
+{
+    public class ConnectionDb
+    {
+        public SqlConnection GetConnection()
+        {
+
+            string connectionString = "Data source = localhost;Initial Catalog=myjdbapp;User=sa;password=Quoctrung61";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
